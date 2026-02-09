696f3f28a7eeb3ac0ad635e39bb5eed1593ecf40
Andrei Bituleanu <andrei.bituleanu@yahoo.com>
2026-02-09 12:58:40 -0500

    frontend UI overhaul

--- src/view/Launcher.tsx
+++ src/view/Launcher.tsx
@@ -191,7 +191,7 @@ export default function Launcher() {
 
           <Divider />
 
-          <div className="miniFeature">
+          <div className="miniFeature previewCard">
             <motion.div
               className="miniFeatureCard"
               initial={{ opacity: 0, y: 18 }}
